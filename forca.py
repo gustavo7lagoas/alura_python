5d09369f6642749459ab067d99993a1709c3fc3e
Gustavo Fonseca <gustavo.fonseca@farfetch.com>
2018-02-26 16:15:55 +0000

    Adiciona fim de jogo

--- forca.py
+++ forca.py
@@ -6,23 +6,37 @@ def jogar():
     print('************ Bem Vindo ao Jogo da Forca *************')
     print('*****************************************************')
 
-    palavra_secreta = 'banana'
+    palavra_secreta = 'banana'.upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
     
-    acertou = False
-    enforcou = False
+    erros = 0
+
+    print(letras_acertadas)
     
-    while(not acertou and not enforcou):
+    while(True):
         chute = input('Informe uma letra: ')
-        chute = chute.strip()
+        chute = chute.strip().upper()
         
         print('Voce escolheu a letra: {}'.format(chute))
 
-        posicao = 0
+        if(chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if(letra.upper() == chute.upper()):
+                    letras_acertadas[posicao] = chute
+                posicao += 1
+        else:
+            erros += 1
+            print('Voce errou. Tentativa {} de 6'.format(erros + 1))
 
-        for letra in palavra_secreta:
-            if(letra.upper() == chute.upper()):
-                print('A letra {} aparece na posicao {}'.format(letra, str(posicao + 1)))
-            posicao += 1
+        print(letras_acertadas)
+
+        if(erros == 6):
+            print('Voce perdeu!')
+            break
+        if('_' not in letras_acertadas):
+            print('Voce ganhou!')
+            break
 
     print('************ Fim do Jogo *************')
 
